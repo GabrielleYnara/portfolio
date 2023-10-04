f809cd449bcecb7c89cb7864ac2c069a6b1a9963
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-03 23:09:31 -0500

    contact me required field verification

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,19 +1,24 @@
-const sendBtn = document.querySelector("#sendBtn");
-
-sendBtn.addEventListener("click", (event) => {
-    const identity = document.querySelector("#identity");
-    const email = document.querySelector("#email");
-    const message = document.querySelector("#message");
-    if (identity.value != "" && email.value != "" && message.value != ""){
-        console.log(identity.value);
-        console.log(email.value);
-        console.log(message.value);
-        identity.value = "";
-        email.value = "";
-        message.value = "";
-    } else {
-        event.preventDefault(); //doesn't reload for now
-        console.log("clicked")
-    }
-    
+const contactForm = document.querySelector(".contact-me");
+  
+contactForm.addEventListener('submit', event => {
+  // Prevent default form submission
+  event.preventDefault();
+  
+  // Check if the form is valid
+  if (contactForm.checkValidity() === false) {
+    event.stopPropagation();
+  } else {
+  // Send email
+  Email.send({
+    SecureToken : "33c98016-0416-476d-bf35-cf3c9f5e0ccf",
+    To : 'gabrielleynara@ymail.com',
+    From : document.querySelector("#email").value,
+    Subject : "Contact from Portfolio",
+    Body : document.querySelector("#message").value
+    }).then(
+      message => alert(message)
+    );
+  }
+  // Add the 'was-validated' class to display feedback
+  contactForm.classList.add('was-validated');
 });
