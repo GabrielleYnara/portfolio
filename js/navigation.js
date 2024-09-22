d9afc38e8eeede31d144e960f83ef9c5685f2982
Gabrielle Ynara <gabrielleynara@ymail.com>
2024-09-22 10:48:43 -0500

    Code clean-up

--- js/navigation.js
+++ js/navigation.js
@@ -1,46 +1,61 @@
+/** This method toggles menu.
+ * When the menu is closed: it hides the menu button and shows the close button instead;
+ * it blurs the rest of the screen, and adds an event listener to remove blur when user clicks or tabs out of menu;
+ * a menu is expanded with a list of items.
+ * When the menu is open: it hides the close button and shows the menu button instead;
+ * the menu collapses, and the blur is removed.
+  */
 function toggleNavMenu(){
-    var nav = document.getElementById("navigation-items");
-    if (nav.classList.contains("open")){ //closes
-        nav.classList.remove("open");
-        closeBtn.hidden = true;
-        navBtn.hidden = false;
-        document.querySelector("main").classList.toggle("blur");
-        document.querySelector("main").removeEventListener("click", toggleNavMenu);
-        lastNavItem.removeEventListener("focusout", toggleNavMenu);
-    } else { //opens
-        nav.classList.add("open");
-        closeBtn.hidden = false;
-        navBtn.hidden = true;
-        document.querySelector("main").classList.toggle("blur");
-        document.querySelector("main").addEventListener("click", toggleNavMenu);
+    let nav = document.getElementById("navigation-items");
+    const lastNavItem = document.getElementById("navigation-items").querySelector('li:last-of-type');
+    let isMenuOpen = nav.classList.contains("open");
+    closeBtn.hidden = isMenuOpen;
+    navBtn.hidden = !isMenuOpen;
+    document.body.classList.toggle("blur");
+    nav.classList.toggle("open");
+
+    if (isMenuOpen){
+        document.body.removeEventListener("click", handleBodyClicks);
+        lastNavItem.removeEventListener("focusout", toggleNavMenu); // toggles the menu when tabbing through the last item
+    } else {
+        document.body.addEventListener("click", handleBodyClicks);
         lastNavItem.addEventListener("focusout", toggleNavMenu);
     }
 }
 
+/** This method closes the menu when clicking outside the menu box or */
+function handleBodyClicks(event){
+    let header = document.querySelector("header");
+    if (!header.contains(event.target) 
+        || event.target.parentNode.classList.contains("external-links")){
+        toggleNavMenu();
+    }
+}
+/** This method adds styling class and accessibility atributes to indicate current page */
 function activePage(element) {
     element.classList.add("active");
     element.setAttribute("aria-current","page");
 }
-
+/** This method removes styling class and accessibility atributes indicating current page */
 function removeActive(element) {
     element.classList.remove("active");
     element.removeAttribute("aria-current");
 }
 
-var currentLocation = location.pathname.split('/').pop(); // retrieves the last part of the path name
+let currentLocation = location.pathname.split('/').pop(); // retrieves the last part of the path name
 const navBtn = document.getElementById("navBtn");
-var navItems = document.querySelectorAll("#navigation-items li");
-var closeBtn = document.getElementById("closeBtn");
-const lastNavItem = document.getElementById("navigation-items").querySelector('li:last-of-type');
+let navItems = document.querySelectorAll("#navigation-items li");
+let closeBtn = document.getElementById("closeBtn");
 
 // Runs through menu items and mark the current page as active
-for ( var i = 0; i < navItems.length; i++) {    
-    if (navItems[i].children[0].href.split('/').pop() === currentLocation) {
-        activePage(navItems[i]);
+navItems.forEach( (item) => {
+    if (item.children[0].href.split('/').pop() === currentLocation) {
+        activePage(item);
     } else {
-        removeActive(navItems[i]);
+        removeActive(item);
     }
-}
+});
+
 
 navBtn.addEventListener("click", toggleNavMenu);
 closeBtn.addEventListener("click", toggleNavMenu);
