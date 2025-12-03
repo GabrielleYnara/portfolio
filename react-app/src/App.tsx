0a10f90f91daa9c3b8e02e1c0608551283a5da35
Gabrielle Ynara <gabrielleynara@ymail.com>
2025-12-03 01:59:25 -0500

    Update navbar to fixed position

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,18 +7,21 @@ import Contact from "./pages/Contact/Contact";
 import Projects from "./pages/Projects/Projects";
 import ProjectDetails from "./pages/Projects/ProjectDetails/ProjectDetails";
 
+import "./App.scss";
+
 function App() {
     return (
         <BrowserRouter>
             <NavBar />
-
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Projects" element={<Projects />} />
-                <Route path="/Projects/ProjectDetails" element={<ProjectDetails />} />
-                <Route path="/Resume" element={<Resume />} />
-                <Route path="/Contact" element={<Contact />} />
-            </Routes>
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Projects" element={<Projects />} />
+                    <Route path="/Projects/ProjectDetails" element={<ProjectDetails />} />
+                    <Route path="/Resume" element={<Resume />} />
+                    <Route path="/Contact" element={<Contact />} />
+                </Routes>
+            </main>
         </BrowserRouter>
     )
 }
--- react-app/src/components/NavBar/NavBar.tsx
+++ react-app/src/components/NavBar/NavBar.tsx
@@ -32,31 +32,32 @@ function NavBar() {
     }
 
     return (
-        <nav className={isMenuOpen ? "open" : ""}>
-            <Link to="/" onClick={handleNavItemClick}>
-                <img src={monogram} alt="Monogram" className="mobile-logo" hidden/>
-                <img src={primaryLogo} alt="Personal Logo" className="desktop-logo" hidden/>
-            </Link>
+        <div id="nav-bar">
+            <nav className={isMenuOpen ? "open" : ""}>
+                <Link to="/" onClick={handleNavItemClick}>
+                    <img src={monogram} alt="Monogram" id="monogram"/>
+                </Link>
 
-            <button id="navBtn" onClick={toggleNavMenu} aria-label="Expand navigation menu" hidden={isMenuOpen}>
-                <img src={menuIcon} />
-            </button>
-            <button id="closeBtn" onClick={toggleNavMenu} aria-label="Close navigation menu" hidden={!isMenuOpen}>
-                <img src={closeIcon} />
-            </button>
-            <button id="blurred-overlay" onClick={handleNavItemClick} aria-hidden="true" tabIndex="-1" hidden={!isMenuOpen}></button>
+                <button id="navBtn" onClick={toggleNavMenu} aria-label="Expand navigation menu" hidden={isMenuOpen}>
+                    <img src={menuIcon} />
+                </button>
+                <button id="closeBtn" onClick={toggleNavMenu} aria-label="Close navigation menu" hidden={!isMenuOpen}>
+                    <img src={closeIcon} />
+                </button>
+                <button id="blurred-overlay" onClick={handleNavItemClick} aria-hidden="true" tabIndex="-1" hidden={!isMenuOpen}></button>
 
-            <div id="navigation-items">
-                <NavLink to="/" onClick={handleNavItemClick}>Home</NavLink>
-                <NavLink to="/Projects" onClick={handleNavItemClick}>Projects</NavLink>
-                <NavLink to="/Resume" onClick={handleNavItemClick}>Resume</NavLink>
-                <NavLink to="/Contact" onClick={handleNavItemClick}>Contact</NavLink>
-                <div id="external-links">
-                    <a href="https://github.com/GabrielleYnara"  onClick={handleNavItemClick} target="_blank" aria-label="Opens GitHub profile in a new tab">GitHub</a>
-                    <a href="https://www.linkedin.com/in/gabrielleynara/" onClick={handleNavItemClick} target="_blank" aria-label="Opens LinkedIn profile in a new tab">LinkedIn</a>
+                <div id="navigation-items">
+                    <NavLink to="/" onClick={handleNavItemClick}>Home</NavLink>
+                    <NavLink to="/Projects" onClick={handleNavItemClick}>Projects</NavLink>
+                    <NavLink to="/Resume" onClick={handleNavItemClick}>Resume</NavLink>
+                    <NavLink to="/Contact" onClick={handleNavItemClick}>Contact</NavLink>
+                    <div id="external-links">
+                        <a href="https://github.com/GabrielleYnara"  onClick={handleNavItemClick} target="_blank" aria-label="Opens GitHub profile in a new tab">GitHub</a>
+                        <a href="https://www.linkedin.com/in/gabrielleynara/" onClick={handleNavItemClick} target="_blank" aria-label="Opens LinkedIn profile in a new tab">LinkedIn</a>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     );
 }
 
