2e709824165a4c18f6781f20358b1d0f0185ce48
Gabrielle Ynara <gabrielleynara@ymail.com>
2025-12-06 18:12:08 -0500

    Add style to project preview component

--- react-app/src/components/ProjectHighlight/ProjectHighlight.tsx
+++ react-app/src/components/ProjectHighlight/ProjectHighlight.tsx
@@ -1,9 +1,21 @@
+import { NavLink } from "react-router-dom";
 import "./ProjectHighlight.scss"
 
 function ProjectHighlight(){
+    const project = {
+        description: "Quick summary of the project Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        demo_url: "#"
+    }
     return <div className="project-highlight">
         <span></span>
-        <p>project description</p>
+        <div>
+            <p>{project.description}</p>
+            <div className="links">
+                <a href={project.demo_url} target="_blank" aria-label="Opens project in a new tab">Demo</a>
+                <NavLink to="/Projects/ProjectDetails">Project details</NavLink>
+            </div>
+            
+        </div>
     </div>
 }
 
