fd0d2c1e2a41d5bbd2a3ebb64399dd95caffe02e
Gabrielle Ynara <gabrielleynara@ymail.com>
2025-12-06 14:04:01 -0500

    Create Call To Action component

---
+++ react-app/src/components/CallToAction/CallToAction.tsx
@@ -0,0 +1,14 @@
+import Button from "../Button/Button"
+
+import "./CallToAction.scss";
+
+function CallToAction() {
+    return <section className="cta">
+        <h1>Do you need a Software Developer?</h1>
+        <p>If you're looking for a software developer who likes to get things done, let's talk.</p>
+        <p className="email"><strong>gabrielleynara@ymail.com</strong></p>
+        <Button button={{title: "Send me a message", class: "golden-button", destination: "/Contact"}}/>
+    </section>
+}
+
+export default CallToAction;
