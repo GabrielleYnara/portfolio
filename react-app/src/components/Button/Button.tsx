33c112fc858f569512ef07315e1b3b6bbdebafac
Gabrielle Ynara <gabrielleynara@ymail.com>
2025-12-06 14:02:06 -0500

    Create golden button

---
+++ react-app/src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom";
+import "./Button.scss"
+
+function Button({button}){
+    return<NavLink to={button.destination} className={button.class} role="button">
+        {button.title}
+    </NavLink>
+}
+
+export default Button;
