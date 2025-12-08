dd2f541f8db32eb60e7acc25f79974c6303b60f8
Gabrielle Ynara <gabrielleynara@ymail.com>
2025-12-08 15:15:49 -0500

    Add images to background

--- react-app/src/pages/Home/components/Hero/Hero.tsx
+++ react-app/src/pages/Home/components/Hero/Hero.tsx
@@ -1,11 +1,29 @@
-import stackedLogo from "../../../../assets/img/stacked-logo.svg";
+import logo from "../../../../assets/img/stacked-logo.svg";
+import puzzlePieces from "../../../../assets/img/puzzle-pieces.svg";
+import mysticCards from "../../../../assets/img/cards.svg";
+import constellation from "../../../../assets/img/constellation.svg";
+import eye from "../../../../assets/img/eye.svg";
+import sparkle from "../../../../assets/img/sparkle1.svg";
+import moon from "../../../../assets/img/moon.svg"
 
 import "./Hero.scss"
 
 function Hero() {
     return <section className="hero">
         <div className="inner-content">
-            <img src={stackedLogo} alt="Personal Logo"/>
+            <div className="config">
+                <a href="#" id="language" class="accent">Português</a>
+                <a href="#">
+                    <img src={moon}/>
+                </a>
+            </div>
+            <img src={mysticCards} aria-hidden id="mystic-cards"/>
+            <img src={puzzlePieces} aria-hidden id="puzzle-pieces"/>
+            <img src={sparkle} aria-hidden id="sparkle"/>
+            <img src={eye} aria-hidden id="eye"/>
+            {/* <img src={} aria-hidden id=""/> */}
+            <img src={constellation} aria-hidden id="constellation"/>
+            <img src={logo} alt="Personal Logo"/>
             <h2>Software Developer specializing in Accessibility</h2>
         </div>
         
