e1fc1cbf9a598c567604decb4fcd9ac80ba34c01
Gabrielle Ynara <gabrielleynara@ymail.com>
2026-01-15 13:30:53 -0500

    Add decorations to hero background

--- react-app/src/pages/Home/components/Hero/Hero.tsx
+++ react-app/src/pages/Home/components/Hero/Hero.tsx
@@ -4,26 +4,31 @@ import mysticCards from "../../../../assets/img/cards.svg";
 import constellation from "../../../../assets/img/constellation.svg";
 import eye from "../../../../assets/img/eye.svg";
 import sparkle from "../../../../assets/img/sparkle1.svg";
-import moon from "../../../../assets/img/moon.svg"
+import moon from "../../../../assets/img/moon.svg";
+import arrow from "../../../../assets/img/arrow.svg";
 
 import "./Hero.scss"
 
 function Hero() {
     return <section className="hero">
         <div className="inner-content">
+
             <div className="config">
                 <a href="#" id="language" className="accent">Português</a>
                 <a href="#">
-                    <img src={moon}/>
+                    <img src={moon} id="moon"/>
                 </a>
             </div>
-            <img src={mysticCards} aria-hidden id="mystic-cards"/>
+            
             <img src={puzzlePieces} aria-hidden id="puzzle-pieces"/>
             <img src={sparkle} aria-hidden id="sparkle"/>
-            <img src={eye} aria-hidden id="eye"/>
-            {/* <img src={} aria-hidden id=""/> */}
+            <img src={arrow} aria-hidden id="arrow"/>
             <img src={constellation} aria-hidden id="constellation"/>
-            <img src={logo} alt="Personal Logo"/>
+            <img src={eye} aria-hidden id="eye"/>
+            <img src={mysticCards} aria-hidden id="mystic-cards"/>
+            {/* <img src={} aria-hidden id=""/> */}
+
+            <img src={logo} alt="Personal Logo" id="logo"/>
             <h2>Software Developer specializing in Accessibility</h2>
         </div>
         
